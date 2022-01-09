c365106987c1ea4c9fd40b5fdc7cc329530b39a8
Zachary Sunberg <zachary.sunberg@colorado.edu>
2022-01-09 14:44:32 -0700

    Moved text to tooltip

--- notebook-drawing/main.js
+++ notebook-drawing/main.js
@@ -32,9 +32,9 @@ define([
     }
 
     function updateCellContents(cell, context) {
-        let text =
-            `Annotations created with the [notebook-drawing extension](https://github.com/nicknytko/notebook-drawing):
-<img src="${context.getCanvasData()}"/>`;
+        let extension_url = "https://github.com/nicknytko/notebook-drawing"
+        let description = "Drawing created with the notebook-drawing extension (github.com/nicknytko/notebook-drawing)"
+        let text =`<img src="${context.getCanvasData()}" title="${description}. Install the extension to edit." alt="${description}"/>`;
         cell.set_text(text);
         /*cell.execute();*/
     }
@@ -43,7 +43,7 @@ define([
         cells.forEach(cell => {
             if (cell.metadata.drawing_enabled) {
                 let text = cell.get_text();
-                let contents = text.split("\"")[1];
+                let contents = text.split("\"")[1]; // this seems fragile, but I don't know of a better way
                 setupCell(cell, contents);
             }
         });
